e3dcd7c51b69f99a5101573138ba268cbab896dc
Gerikk <al.blt@protonmail.com>
2022-02-28 11:12:36 +0100

    Débit et fuite

--- src/fr/bath/Baignoire.java
+++ src/fr/bath/Baignoire.java
@@ -4,11 +4,27 @@ public class Baignoire {
 
     private final int volumeMax;
     private int volume;
+    private int volumeFuite;
 
     public Baignoire(int volumeMax) {
         this.volumeMax = volumeMax;
         this.volume = 0;
+        this.volumeFuite = 0;
     }
+
+    public Baignoire(int volumeMax, int volumeFuite) {
+        this.volumeMax = volumeMax;
+        this.volume = 0;
+        this.volumeFuite = volumeFuite;
+    }
+
+    public void fuite(){
+        while (this.volume > 0){
+            this.volume = this.volume - this.volumeFuite;
+            System.out.println("Fuite ! La baignoire est a : " + this.volume);
+        }
+    };
+
 
     public int getVolume() {
         return volume;
--- src/fr/bath/Main.java
+++ src/fr/bath/Main.java
@@ -2,8 +2,10 @@ package fr.bath;
 
 public class Main {
     public static void main(String[] args) {
-        Baignoire baignoire = new Baignoire(1000);
+        Baignoire baignoire = new Baignoire(1000, 10);
         Robinet robinet = new Robinet(baignoire, 50);
+
         robinet.debite();
+        baignoire.fuite();
     }
 }
